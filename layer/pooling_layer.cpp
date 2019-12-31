c1f18fa1e594a99c3059fde5f39e7e8cba8f1117
Swocky <404392659@qq.com>
2019-12-31 14:04:01 +0800

    add some codes to pooling_layer

--- layer/pooling_layer.cpp
+++ layer/pooling_layer.cpp
@@ -1,2 +1,21 @@
 #include "pooling_layer.hpp"
 
+PoolingLayer::PoolingLayer() {}
+
+void PoolingLayer::init(unsigned const _pooling_size,
+                        unsigned const _input_num,
+		                float* _x,
+                        float* _p_gradient,
+		                unsigned const _batch_size) {
+    
+}
+    
+void PoolingLayer::~PoolingLayer(){}
+
+void PoolingLayer::feedForward() {
+
+}
+
+void PoolingLayer::backprop() {
+    
+}
